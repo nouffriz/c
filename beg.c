805f074b88e526db8cb4e21eaf236c457828b93d
nouffriz <34641721+nouffriz@users.noreply.github.com>
2017-12-18 13:04:06 +0530

    Create beg.c

---
+++ beg.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+					char a[]="sachintendulkar";
+					char b[]="littlemaster";
+					int c= 18426;
+					double h=44.63;
+					int e=463;
+					printf("%s\n",a);
+					printf("%s\n",b);
+					printf("%d\n",c);
+					printf("%0.2f\n",h);
+					printf("%d\n",e);
+					return 0;
+					
+					
+					
+	
+}
