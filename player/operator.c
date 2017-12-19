2aa6e4e41b3edc560f4c57980d7fbfb86b1883cb
nouffriz <34641721+nouffriz@users.noreply.github.com>
2017-12-19 10:24:03 +0530

    Add files via upload

---
+++ player/operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+		int a=5;
+		int b=5;
+		printf("%d\t,",a+b);
+		printf("%d\t,",a-b);
+		printf("%d\t,",a*b);
+		printf("%d\t,",a/b);
+		printf("%d\t",a%b);
+	return 0;
+}
